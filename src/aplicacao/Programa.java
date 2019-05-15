03b4e94afa265128bad9da7824b009f9dfaaf76e
Ilton <xlsonplay@gmail.com>
2019-05-15 16:06:23 -0300

    Instanciando a EntityManager e o Factory para conversar com o banco de dados

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,5 +1,9 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
@@ -7,8 +11,11 @@ public class Programa {
 	public static void main(String[] args) {
 		
 		Pessoa p1 = new Pessoa(1,"Ezequiel Santana", "EzeAna@gmail.com");
-		Pessoa p2 = new Pessoa(2,"Fabio mello", "famello@gmail.com");
+		Pessoa p2 = new Pessoa(2, "Fabio mello", "famello@gmail.com");
 		Pessoa p3 = new Pessoa(3,"Elian morais", "Elimorais@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
 		
 		System.out.println(p1);
 		System.out.println(p2);
