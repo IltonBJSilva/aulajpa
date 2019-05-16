a57f16c8f188f97a9f08583599895b3be98908c6
Ilton <xlsonplay@gmail.com>
2019-05-16 15:29:05 -0300

    Realiza persistencia no banco de dados phpmyadmin

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -10,16 +10,23 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1,"Ezequiel Santana", "EzeAna@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Fabio mello", "famello@gmail.com");
-		Pessoa p3 = new Pessoa(3,"Elian morais", "Elimorais@gmail.com");
+		Pessoa p1 = new Pessoa(null,"Ezequiel Santana", "EzeAna@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Fabio mello", "famello@gmail.com");
+		Pessoa p3 = new Pessoa(null,"Elian morais", "Elimorais@gmail.com");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		//Iniciar a transação
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		//Confirmar as alterções que eu fiz
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+		System.out.println("Pronto!!! ");
 
 	}
 
